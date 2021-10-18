024e16d13db6d4395aab181ddcab81324097085d
Komal1897 <55304099+Komal1897@users.noreply.github.com>
2021-10-18 10:41:36 +0530

    Create StringCalculatorShould.java

--- src/test/java/calculator/StringCalculatorShould.java
+++ src/test/java/calculator/StringCalculatorShould.java
@@ -5,16 +5,81 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
 class StringCalculatorShould {
-
-    @Test
-    void empty_string_should_return_0() {
-        StringCalculator stringCalculator = new StringCalculator();
+    
+	@Test
+	void empty_string_should_return_0() {
+	    StringCalculator stringCalculator = new StringCalculator();
         assertEquals(0, stringCalculator.add(""));
     }
 
     @Test
-    void string_with_single_number_should_return_number_as_int() {
-        StringCalculator stringCalculator = new StringCalculator();
-        assertEquals(1, stringCalculator.add("1"));
+    void string_with_single_number_should_return_number() {
+       StringCalculator stringCalculator = new StringCalculator();
+         assertEquals(1, stringCalculator.add("1"));
     }
+    
+    @Test
+    void should_return_sum_of_two_numbers() {
+    	StringCalculator stringCalculator = new StringCalculator();
+         assertEquals(3, stringCalculator.add("1,2"));
+    }
+
+    @Test
+    void should_return_sum_of_unknown_amount_of_numbers() {
+    StringCalculator stringCalculator = new StringCalculator();
+     assertEquals(6, stringCalculator.add("1,2,3"));
+    }
+
+    @Test
+    void should_return_sum_of_numbers_with_commas_and_new_lines() {
+        StringCalculator stringCalculator = new StringCalculator();
+         assertEquals(6, stringCalculator.add("1,2\n3"));
+         assertEquals(3, stringCalculator.add("1\n2"));
+    }
+         
+    @Test
+     void should_return_sum_of_numbers_split_by_custom_delimiter() {
+             StringCalculator stringCalculator = new StringCalculator();
+              assertEquals(3, stringCalculator.add("//;\n1;2"));
+              assertEquals(6, stringCalculator.add("//,\n2,3,1"));
+     }
+    
+       
+    @Test
+    void should_throws_Exception_On_NegativeNumber() {
+            StringCalculator stringCalculator = new StringCalculator();
+           assertThrows(IllegalArgumentException.class,()->{
+            stringCalculator.add("-3");});
+    }
+    
+   
+	@Test
+	void should_throws_On_Negative_Numbers_With_All_Numbers_In_ExceptionMessage() {
+	    StringCalculator stringCalculator = new StringCalculator();
+	    assertThrows(IllegalArgumentException.class,()->{
+	    stringCalculator.add("1,-3,5,-5,-13");});
+	}
+	
+	@Test
+    void should_maps_Number_Above_1000_To_Last_Three_Digits(){
+        StringCalculator stringCalculator = new StringCalculator();
+         assertEquals(2, stringCalculator.add("1002"));
+         assertEquals(42, stringCalculator.add("1040,1002"));
+    }
+
+	
+	@Test
+    void should_accepts_Delimiter_Of_Arbitrary_Length() {
+        StringCalculator stringCalculator = new StringCalculator();
+         assertEquals(6, stringCalculator.add("//[***]\n1***2***3"));
+    }
+	
+	@Test
+    void should_accepts_Multiple_Delimiter() {
+        StringCalculator stringCalculator = new StringCalculator();
+         assertEquals(6, stringCalculator.add("//[*][%]\n1*2%3"));
+         assertEquals(6, stringCalculator.add("//[**][%%]\n1**2%%3"));
+    }
+	
+
 }
