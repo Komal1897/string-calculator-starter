70a8beef14bb4d93be49c53dcb55f7adaf4c39ff
Komal1897 <55304099+Komal1897@users.noreply.github.com>
2021-10-18 11:05:21 +0530

    Update StringCalculatorShould.java

--- src/test/java/calculator/StringCalculatorShould.java
+++ src/test/java/calculator/StringCalculatorShould.java
@@ -6,15 +6,15 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class StringCalculatorShould {
     
-	@Test
-	void empty_string_should_return_0() {
-	    StringCalculator stringCalculator = new StringCalculator();
-        assertEquals(0, stringCalculator.add(""));
+    @Test
+     void empty_string_should_return_0() {
+     	StringCalculator stringCalculator = new StringCalculator();
+     	assertEquals(0, stringCalculator.add(""));
     }
 
     @Test
     void string_with_single_number_should_return_number() {
-       StringCalculator stringCalculator = new StringCalculator();
+       	 StringCalculator stringCalculator = new StringCalculator();
          assertEquals(1, stringCalculator.add("1"));
     }
     
@@ -26,8 +26,8 @@ class StringCalculatorShould {
 
     @Test
     void should_return_sum_of_unknown_amount_of_numbers() {
-    StringCalculator stringCalculator = new StringCalculator();
-     assertEquals(6, stringCalculator.add("1,2,3"));
+    	StringCalculator stringCalculator = new StringCalculator();
+    	 assertEquals(6, stringCalculator.add("1,2,3"));
     }
 
     @Test
@@ -40,15 +40,15 @@ class StringCalculatorShould {
     @Test
      void should_return_sum_of_numbers_split_by_custom_delimiter() {
              StringCalculator stringCalculator = new StringCalculator();
-              assertEquals(3, stringCalculator.add("//;\n1;2"));
-              assertEquals(6, stringCalculator.add("//,\n2,3,1"));
+             assertEquals(3, stringCalculator.add("//;\n1;2"));
+             assertEquals(6, stringCalculator.add("//,\n2,3,1"));
      }
     
        
     @Test
     void should_throws_Exception_On_NegativeNumber() {
             StringCalculator stringCalculator = new StringCalculator();
-           assertThrows(IllegalArgumentException.class,()->{
+            assertThrows(IllegalArgumentException.class,()->{
             stringCalculator.add("-3");});
     }
     
@@ -64,7 +64,11 @@ class StringCalculatorShould {
 	void should_return_GetCalledCount() throws Exception
 	{
 		StringCalculator stringCalculator = new StringCalculator();
-		assertEquals(
+		assertEquals(6, stringCalculator.add("//[**][%%]\n1**2%%3"));
+		assertEquals(6, stringCalculator.add("//[**][%%]\n1**2%%3"));
+		assertEquals(1, stringCalculator.add("1"));
+		assertEquals(3,stringCalculator.GetCalledCount());
+	}
 	
 	@Test
     void should_maps_Number_Above_1000_To_Last_Three_Digits(){
